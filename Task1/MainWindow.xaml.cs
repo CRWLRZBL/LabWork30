f13c44229647b7274f375f6cfa62d7f2fc977b7c
Crowler 1337 <gamevax25@gmail.com>
2024-06-24 15:46:42 +0300

    Initial commit

---
+++ Task1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Task1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AgreeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            SubsButton.IsEnabled = true;           
+        }
+        private void AgreeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SubsButton.IsEnabled = false;         
+        }
+
+        private void SubsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Task4/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Task4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FontSizeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            UserInputTextBox.FontSize = Convert.ToInt32(radioButton.Content);
+        }
+
+        private void AlignmentRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (LeftHARB.IsChecked == true)
+            {
+                UserInputTextBox.TextAlignment = TextAlignment.Left;
+            }
+            else if (CenterHARB.IsChecked == true)
+            {
+                UserInputTextBox.TextAlignment = TextAlignment.Center;
+            }
+            else
+            {
+                UserInputTextBox.TextAlignment = TextAlignment.Right;
+            }
+        }
+    }
+}
